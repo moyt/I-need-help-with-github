97df93fc15a96c17a400d1585cc1f65c37a4f24f
moises terrazas <moyt90@gmail.com>
2017-07-04 22:59:48 -0700

    yep its all good
    the html and css supplied is not that good but oh well it's whatevs.  the javascript works fine and i have plenty of stuff in my quotes array

---
+++ js/fisheryates.js
@@ -0,0 +1,29 @@
+//FISHER YATES!  some super old technique to shuffle arrays
+//thanks Youtube User Adam Khoury and this stackoverflow page https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+
+
+function shuffleArray(array) {
+
+    //make my variables for my function
+    var currentIndex = array.length;
+    var temporaryValue;
+    var randomIndex;
+
+    // Keep looping through array as long as it has something in it and do stuff
+    while (currentIndex > 0) {
+
+        // Pick a random number and subtract one from currentIndex
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        //put the last item in array in a temporary value
+        temporaryValue = array[currentIndex];
+        
+        //simply make the random number we pulled the index number for our array and equal it to the last item in the array
+        array[currentIndex] = array[randomIndex];
+        //make the last item equal to the random number index
+        array[randomIndex] = temporaryValue;
+    }
+    //return my new shuffled array :)
+    return array;
+} //end shuffleArray();
--- js/script.js
+++ js/script.js
@@ -1,97 +1,172 @@
 var quotes = [
     {
-        quote: "yellow",
-        source: "fish",
-        citation: "sunami",
-        year: "1994"
+        //lyric
+        quote: "I don't mean to seem like I care about material things <br> Like a social status <br> I just want four walls and adobe slabs for my girls",
+
+        artist: "Animal Collective",
+
+        //track
+        source: "My Girls",
+
+        //album
+        citation: "Merriweather Post Pavilion",
+        
+        //year
+        date: "2009",
+
+        genre: "electronic",
+
+        bpm: "120",
+
     },
     {
-        quote: "orange",
-        source: "gravy",
-        citation: "midwest",
-        year: "19922"
+        quote: "Don't you know that<br>When I see your face<br>It's like sunlight dripping",
+        artist: "Bag Raiders",
+        source: "Sunlight",
+        citation: "Bag Raiders",
+        date: "2010",
+        genre: "electronic",
+        bpm: "125",
     },
     {
-        quote: "fever",
-        source: "speaker",
-        citation: "NY",
-        year: "20XX"
+        quote: "Push me<br>And then just touch me<br>Till I can get my satisfaction",
+        artist: "Benny Benassi",
+        source: "Satisfaction",
+        citation: "Hypnotica",
+        date: "2003",
+        genere: "electronic",
+        bpm: "130"
     },
     {
-        quote: "spiked",
-        source: "palm",
-        citation: "Miami",
-        year: "1983"
+        quote: "All is full of love, all is full of love",
+        artist: "Bj&#246rk",
+        source: "All Is Full Of Love",
+        citation: "Homogenic",
+        date: "1997",
+        genere: "IDM",
+        bpm: "76"
+    },
+    {
+        quote: "Another world will surround me<br>Another heart will forgive",
+        artist: "The Chemical Brothers",
+        source: "Another World",
+        citation: "Further",
+        date: "2010",
+        genere: "electronic",
+        bpm: "119"
+    },
+    {
+        quote: "We dissolve everything<br>Scratch follicles so they don’t grow<br>Expose your shame for all to see<br>Sell your bones as ivory",
+        artist: "Crystal Castles",
+        source: "Insulin",
+        citation: "(III)",
+        date: "2012",
+        genere: "electronic",
+        bpm: "123"
+
+    }, {
+        quote: "Kiss, suddenly alive<br>Happiness arrive<br>Hunger like a storm<br>How do I begin?",
+        artist: "Daft Punk",
+        source: "Touch (feat. Paul Williams)",
+        citation: "Random Access Memories",
+        date: "2013",
+        genere: "electronic",
+        bpm: "91"
+
+    },
+    {
+        quote: "I'm gonna take a stab at this<br>Sure you will be alright<br>Make a decision with a kiss<br>Baby I have false spite",
+        artist: "Grizzly Bear",
+        source: "Ready, Able",
+        citation: "Veckatimest",
+        date: "2009",
+        genere: "Rock",
+        bpm: "153"
     }
+
+
+    /*
+    {
+        quote: "",
+        artist: "",
+        source: "",
+        citation: "",
+        date: "",
+        genere: "",
+        bpm: ""
+    }
+    */
+
 ];
 
 
 
-//first off i want to create an empty array and create var poppedQuoteIndex which will be used later.
+//first off i want to create an empty array and initialize a handful of other variables.
 var tmpArray = [];
 var poppedQuoteIndex;
+var milliSeconds = 10000;
+var printDelay;
 
-//create a function that populates my tmpArray with however many quotes there are in quotes[] and also randomized the array;
+
+
+//this function populates my empty array with however many objects i have in my quotes array in the form of integers and then shuffles them. that sentence is too long.
 function populateAndRandomize() {
 
     //so i need a loop to iterate and add to my empty array.  pretty easy stuff.
     for (var i = 0; i < quotes.length; i++) {
         tmpArray.push(i);
     }
-    //and shuffle my array by calling shuffleArray from snippets.js (i'm sure i could just move over the code here but for now it's not a huge concern)
+    //and shuffle my array by calling shuffleArray from fisheryates.js (i'm sure i could just move over the code here but for now it's not a huge concern)
     shuffleArray(tmpArray);
 }
 
 
+//this function actually draws a number and not a quote
 function getRandomQuote() {
-    
-
-    
-    //first just check to see if theres any thing in my tmpArray.  if not then populate it by calling my populateAndRandomize() function.
+    //check to see if theres any thing in my tmpArray.  if not then populate it by calling my populateAndRandomize() function.
     if (tmpArray.length <= 0) {
-        
-            console.log("shuffled");
+
+        console.log("shuffled");
         populateAndRandomize();
 
-        //this checks to see the laast used array index from poppedQuoteIndex is the same as the last item in my array.  This is for making sure quotes dont repeat themselves.  
+        //this checks to see the laast used array index from poppedQuoteIndex is the same as the last item in my array.  This is for making sure quotes dont repeat themselves.
         if (poppedQuoteIndex == tmpArray[tmpArray.length - 1]) {
             console.log("DOUBLE");
-            
+
+            //create two variables that are going to be defined with the first item in array and last item in array
             var tmpFirstItem;
             var tmpLastItem;
-            
+
             //grab first item
             tmpFirstItem = tmpArray.shift();
             //grab last item
             tmpLastItem = tmpArray.pop();
-            
-            console.log(tmpFirstItem + " " + tmpLastItem)
-            
+
+            console.log(tmpFirstItem + " switched with " + tmpLastItem)
+
             //push first item to the end of the line
             tmpArray.push(tmpFirstItem);
-            
+
             //bring last item to the begining of the line
             tmpArray.unshift(tmpLastItem);
-        }
-    }
+            //CAUTION: theres a much easier way to do the swapping but it works for now so i'm not messing with it.
 
+        } //end doubles checker
+    } //end populate and shuffle
 
+    //by now My temp array is populated, randomized, and checked for doubles.  grab the last item of my array, store it in a variable, and then get rid of item from array.  
 
-    //by now the array is populated, randomized, and checked for doubles.  grab the last item of my array, store it in a variable, and then get rid of item from array.  
-    
     poppedQuoteIndex = tmpArray.pop();
     return poppedQuoteIndex;
+
 }
 
 
-//Background color Section =================================================
-//==========================================================================
 
-
-
-//funciton to spit out a "random" color in HSL
-
+//funciton change background to a "random" color in HSL
+//want to keep it dark so i will just leave the saturation in the middle and lightness low.  Hue can be whatevs and random
 function randomBackgroundColor() {
+    //grab a random hue value from 1 to 360,  grab body selector, and change it's style.
     var randomH = Math.floor(Math.random() * 360) + 1;
     var pageBackgroundColor = document.querySelector("body");
     pageBackgroundColor.style.backgroundColor = "hsl(" + randomH + ", 60%, 30%)";
@@ -101,44 +176,42 @@ function randomBackgroundColor() {
 
 
 
-var myVar;
 
-//this is the stuff that's gunna happen when that button gets pushed
+//this is the stuff that's gunna happen when dat button gets pushed
 function printQuote() {
-    
-    
+
+    //change background color
     randomBackgroundColor();
-    clearInterval(intervalID);
-    clearTimeout(myVar);
-    
+
+    //clear printDelay which will be re-activated at the end of this function if it's present. at first it wont clear anything since setTimeout hasnt started
+    clearTimeout(printDelay);
+
+
+    //run getRandomQuote and store it in grabRandomQuote
     var grabRandomQuote = getRandomQuote();
 
 
-
-
-    //associate last item with quote index and print it
-    
-    
-
+    //grab the HTML elements for quote and source
     var quoteClass = document.getElementsByClassName("quote")[0];
     var sourceClass = document.getElementsByClassName("source")[0];
-    
+    var artistClass = document.getElementsByClassName("artist")[0];
+
     console.log(poppedQuoteIndex);
     console.log(tmpArray);
-    //console.log("this many items left in my array " + tmpArray.length);
-    
-    
+
+    //print associated grabRandomQuote with quotes index in the HTML
     quoteClass.innerHTML = quotes[grabRandomQuote].quote;
-    sourceClass.innerHTML = quotes[grabRandomQuote].source;
-    
-    
-    myVar = setTimeout(printQuote, 2000);
-    
+    artistClass.innerHTML = quotes[grabRandomQuote].artist + " ";
+    sourceClass.innerHTML = " " + quotes[grabRandomQuote].source;
+
+    //start printDelay so that it prints again in specified seconds.
+    printDelay = setTimeout(printQuote, milliSeconds);
+
 
 }
+printQuote();
 
 
-var intervalID = window.setInterval(printQuote, 2000);
 
 
 
